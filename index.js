547090fcd7a289e37e2b02b232fb9d3b74fa6994
Dev Tyagi <devutyagi2001@gmail.com>
2023-12-22 10:38:53 +0530

    Negotiation Engine

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('http');
+const { join } = require('path');
+const { Server } = require('socket.io');
+const { LanguageServiceClient } = require('@google-cloud/language');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const languageClient = new LanguageServiceClient();
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
